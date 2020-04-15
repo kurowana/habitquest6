47d077e29f24aa47e945d201ae9520a5e8dc85ea
kurowana <blackwannabe1024@gmail.com>
2020-04-15 21:15:43 +0900

    20200415作業分

--- resources/js/components/pages/WelcomeBase.vue
+++ resources/js/components/pages/WelcomeBase.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>welcome</div>
+  <div>welcome3</div>
 </template>
 
 <script>
--- resources/js/components/units/GameUnit.vue
+++ resources/js/components/units/GameUnit.vue
@@ -1,9 +1,55 @@
 <template>
-    <div>game</div>
+  <div class="bg">
+    <p class="view">game</p>
+    {{ this.width }}
+    <div v-if="testShow">test</div>
+  </div>
 </template>
 
+
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      test: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  },
+  computed: {
+    testShow: function() {
+      if (this.width > 1000) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  methods: {
+    handleResize: function() {
+      // resizeのたびにこいつが発火するので、ここでやりたいことをやる
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+    }
+  },
+  mounted: function() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  beforeDestroy: function() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+};
 </script>
 
-<style></style>
+<style scoped>
+.bg {
+  width: 100%;
+  height: 100%;
+  background: url("../../../../storage/app/public/bg_img/tree.jpg") center
+    center no-repeat;
+  background-size: cover;
+}
+.view {
+  font-size: 1vw;
+}
+</style>
