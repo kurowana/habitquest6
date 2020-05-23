8b5fac933a04aa3809bf91c8d9863926562a6186
kurowana <blackwannabe1024@gmail.com>
2020-05-23 18:18:49 +0900

    20200523作業分

---
+++ resources/js/components/atoms/CommandButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="command-button" @click.stop="doClickEvent">{{commandText}}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    commandText: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    doClickEvent: function() {
+      this.$emit("click-event");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.command-button {
+  width: 100px;
+  height: 50px;
+  background: black;
+  color: white;
+}
+</style>
---
+++ resources/js/components/molecules/CommandMenu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <command-button :command-text="'攻撃'" @click-event="setCommand('attack')"></command-button>
+    <command-button :command-text="'魔法'" @click-event="setCommand('magic')"></command-button>
+  </div>
+</template>
+
+<script>
+import CommandButton from "../atoms/CommandButton";
+
+export default {
+  components: {
+    "command-button": CommandButton
+  },
+  methods: {
+    setCommand: function(command) {
+      this.$emit("recive-command", command);
+    }
+  }
+};
+</script>
